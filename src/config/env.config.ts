1eb69b37719f20dabdaf19949868fb3d46a44cc5
Carlos Eduardo <desousapazc@gmail.com>
2025-11-28 10:34:12 -0300

    Atualiza arquivos de configuração

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -11,6 +11,7 @@ export const config = {
   nodeEnv: process.env.NODE_ENV || 'development',
   port: parseInt(process.env.PORT || '3000', 10),
   jwtSecret: process.env.JWT_SECRET || 'SUPER_SECRETO_PARA_JWT',
+
   database: {
     host: process.env.DB_HOST || 'localhost',
     port: parseInt(process.env.DB_PORT || '5432', 10),
@@ -19,7 +20,9 @@ export const config = {
     name: process.env.DB_DATABASE || 'travelar',
     ssl: process.env.DB_SSL === 'true',
   },
+
   corsOrigin: process.env.CORS_ORIGIN || 'http://localhost:5173',
+
   cloudinary: {
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
