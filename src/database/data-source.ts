6e828e5b8ad1d023f8e6ed8bb4c2fc9bb4f4e4a2
Carlos Eduardo <desousapazc@gmail.com>
2025-12-01 13:35:22 -0300

    fix: Últimas modificações no backend para a apresentação final

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -4,9 +4,12 @@ import { Usuario } from '../Usuario/entity';
 import { Imovel } from '../Imovel/entity';
 import { Reserva } from '../Reserva/entity';
 import { Avaliacao } from '../Avaliacao/entity';
-import path from 'path'; // <--- Necessário para achar as migrations
+import path from 'path';
 
 const isProduction = config.nodeEnv === 'production';
+
+const sslConfig =
+  process.env.DB_SSL === 'true' || isProduction ? { rejectUnauthorized: false } : false;
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
@@ -15,12 +18,11 @@ export const AppDataSource = new DataSource({
   username: config.database.username,
   password: config.database.password,
   database: config.database.name,
-
   synchronize: !isProduction,
   logging: false,
   entities: [Usuario, Imovel, Reserva, Avaliacao],
   migrations: [path.join(__dirname, '/../database/migrations/*.{ts,js}')],
   migrationsRun: isProduction,
   subscribers: [],
-  ssl: isProduction ? { rejectUnauthorized: false } : false,
+  ssl: sslConfig,
 });
